0a9f43128d75e2fa070928cd93a430056696a767
André Jaenisch <andre.jaenisch@posteo.de>
2020-06-14 17:29:33 +0200

    Move source code into src and adjust paths
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,34 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import copy from 'rollup-plugin-copy'
 import { terser } from 'rollup-plugin-terser'
 
+const copyOptions = {
+  targets: [
+    { src: './src/logo', dest: './dist' },
+    { src: './src/modal', dest: './dist' },
+    { src: './src/manifest.json', dest: './dist/' }
+  ]
+}
+
 export default [{
-  input: 'background/index.js',
+  input: './src/background/index.js',
   output: [{
-    file: 'dist/background.js',
+    file: './dist/background.js',
     format: 'iife',
     name: 'vulncost'
   }, {
-    file: 'dist/background.umd.js',
+    file: './dist/background.umd.js',
     format: 'umd',
     name: 'vulncost'
   }, {
-    file: 'dist/background.es6.js',
+    file: './dist/background.es6.js',
     format: 'es'
   }],
   plugins: [
     resolve(),
     commonjs(),
-    terser()
+    terser(),
+    copy(copyOptions)
   ]
 }]
