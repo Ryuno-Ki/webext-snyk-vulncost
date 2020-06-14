c0579743364ec5c80196b1185ed5f073b50d7d58
André Jaenisch <andre.jaenisch@posteo.de>
2020-06-14 11:51:43 +0200

    Setup rollup.

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+export default [{
+  input: 'background/index.js',
+  output: [{
+    file: 'dist/background.umd.js',
+    format: 'umd',
+    name: 'vulncost'
+  }, {
+    file: 'dist/background.es6.js',
+    format: 'es'
+  }],
+  plugins: [
+    resolve(),
+    commonjs(),
+    terser()
+  ]
+}]
